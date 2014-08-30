c694a4dc7f5eefe5aa6b809959aecc53838ca6a2
Andy Carmichael <AndrewCarmichael2013@u.northwestern.edu>
2014-08-30 11:57:41 -0500

    Initial Commit

---
+++ GitTest/ACViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACViewController.h
+//  GitTest
+//
+//  Created by Andrew Carmichael on 8/30/14.
+//  Copyright (c) 2014 Andrew Carmichael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACViewController : UIViewController
+
+@end
---
+++ GitTest/ACViewController.m
@@ -0,0 +1,29 @@
+//
+//  ACViewController.m
+//  GitTest
+//
+//  Created by Andrew Carmichael on 8/30/14.
+//  Copyright (c) 2014 Andrew Carmichael. All rights reserved.
+//
+
+#import "ACViewController.h"
+
+@interface ACViewController ()
+
+@end
+
+@implementation ACViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
