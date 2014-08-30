862373832a687b432580145da735e33b5646ea4b
Andy Carmichael <AndrewCarmichael2013@u.northwestern.edu>
2014-08-30 12:20:35 -0500

    added a test class

---
+++ ACTest.h
@@ -0,0 +1,13 @@
+//
+//  ACTest.h
+//  GitTest
+//
+//  Created by Andrew Carmichael on 8/30/14.
+//  Copyright (c) 2014 Andrew Carmichael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACTest : NSObject
+
+@end
---
+++ ACTest.m
@@ -0,0 +1,13 @@
+//
+//  ACTest.m
+//  GitTest
+//
+//  Created by Andrew Carmichael on 8/30/14.
+//  Copyright (c) 2014 Andrew Carmichael. All rights reserved.
+//
+
+#import "ACTest.h"
+
+@implementation ACTest
+
+@end
